6f1bad9ebc1c7b3af64691c7509bb5d6de360da5
Oracle-E3 <daniel_cain8@outlook.com>
2025-01-17 13:23:10 -0800

    Got download working

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
+use regex::Regex;
 
 fn main() {
     let bedrock_server_url: &str = "https://www.minecraft.net/en-us/download/server/bedrock";
     let body = reqwest::blocking::get(bedrock_server_url).unwrap().text().unwrap();
-    println!("body = {body:?}");
+    // println!("body = {body:?}");
+    let latest_download_re = Regex::new(r"https?://[a-zA-Z0-9\-\./]+/bin-(?<os>\w+)/bedrock-server-(?<version>[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+).zip").unwrap();
+    let Some(results) = latest_download_re.captures(body.as_str()) else {
+        println!("Can't find download link!");
+        return;
+    };
+    println!("OS: {} - Version {}", &results["os"], &results["version"]);
 }
